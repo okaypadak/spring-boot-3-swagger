174debacc3cfcfd3e67ac170580f440079428a50
kypdk@proton.me <kypdk@proton.me>
2024-01-27 00:27:59 +0300

    Swagger yapılandırılması tamamlandı, get metodu eklendi

--- src/main/java/dev/padak/backend/BackendApplication.java
+++ src/main/java/dev/padak/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package dev.padak.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/dev/padak/backend/controller/TestController.java
@@ -0,0 +1,32 @@
+package dev.padak.backend.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @Operation(
+            summary = "Retrieve a Tutorial by Id",
+            description = "Get a Tutorial object by specifying its id. The response is Tutorial object with id, title, description and published status.",
+            tags = { "tutorials" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+            @ApiResponse(responseCode = "404", description = "Customer not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)) }) })
+    @GetMapping("/hello")
+    public String getWithSwagger() {
+        return "Hello, world!";
+    }
+}
